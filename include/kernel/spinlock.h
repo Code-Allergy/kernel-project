1c02445923583e1083bc7cf15d4045b06ef0a1fc
Ryan Schaffer <rys686@mail.usask.ca>
2025-02-22 08:52:35 -0600

    added basic spinlocks

---
+++ include/kernel/spinlock.h
@@ -0,0 +1,19 @@
+#ifndef KERNEL_SPINLOCK_H
+#define KERNEL_SPINLOCK_H
+
+#include <stdint.h>
+
+// spinlock struct supporting both irqless and irqfull spinlocks
+typedef struct {
+    volatile uint32_t lock;
+    uint32_t cpsr;
+} spinlock_t;
+
+// initial value that a spinlock should have
+#define SPINLOCK_INIT { .lock = 0, .cpsr = 0 }
+
+inline void spinlock_init(spinlock_t *lock);
+void spinlock_acquire(spinlock_t *lock);
+void spinlock_release(spinlock_t *lock);
+
+#endif // KERNEL_SPINLOCK_H
---
+++ kernel/src/spinlock.c
@@ -0,0 +1,24 @@
+#include <kernel/spinlock.h>
+
+inline void spinlock_init(spinlock_t *lock) {
+    lock->lock = 0;
+    lock->cpsr = 0;
+}
+
+void spinlock_acquire(spinlock_t *lock) {
+    // save current cspr and disable IRQs
+    uint32_t cpsr;
+    __asm__ __volatile__("mrs %0, cpsr\n cpsid i" : "=r" (cpsr) : : "memory");
+    lock->cpsr = cpsr;
+
+    // stomically acquire the lock
+    while (__atomic_test_and_set(&lock->lock, __ATOMIC_ACQUIRE)) {
+        // does nothing on single core machine
+        __asm__ __volatile__("yield");
+    }
+}
+
+void spinlock_release(spinlock_t *lock) {
+    __atomic_clear(&lock->lock, __ATOMIC_RELEASE);
+    __asm__ __volatile__("msr cpsr_c, %0" : : "r" (lock->cpsr));
+}
